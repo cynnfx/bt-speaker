a35f69b02555967625c325f697c217f187ff67de
Charles <charles.oudin@hotmail.fr>
2023-11-03 18:48:41 +0100

    service

--- service_manager.py
+++ service_manager.py
@@ -8,7 +8,7 @@ import subprocess
 # Use the Broadcom SOC Pin numbers
 # Setup the Pin with Internal pullups enabled and PIN in reading mode.
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(2, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(2, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 # Our function on what to do when the button is pressed
 def service_restart(channel):
@@ -16,14 +16,14 @@ def service_restart(channel):
     if status == 0:
         cmd = "sudo systemctl stop bt_speaker.service"
         subprocess.call(["ogg123 /usr/share/sounds/freedesktop/stereo/service-logout.oga"], shell=True)
+        subprocess.call([cmd], shell=True)
     else:
         cmd = "sudo systemctl start bt_speaker.service"
-        subprocess.call(["ogg123 /usr/share/sounds/freedesktop/stereo/service-login.oga"], shell=True)
-    subprocess.call([cmd], shell=True)
-    # time.sleep(2)
+        subprocess.call([cmd], shell=True)
+        subprocess.call(["ogg123 /usr/share/sounds/freedesktop/stereo/service-login.oga"], shell=True)        # time.sleep(2)
 
 # Add our function to execute when the button pressed event happens
-GPIO.add_event_detect(2, GPIO.FALLING, callback = service_restart, bouncetime = 1000)
+GPIO.add_event_detect(2, GPIO.RISING, callback = service_restart, bouncetime = 200)
 
 # Now wait!
 while 1:
